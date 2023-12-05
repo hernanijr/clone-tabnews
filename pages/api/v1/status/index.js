be3fe7b31ea675af410c01e7964bf9cf5d837b8b
Hernani Junior <hernani.junior13@gmail.com>
2023-12-05 22:39:22 +0000

    add try/catch and add status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,31 @@
-import database from "infra/database.js"
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
-  console.log(result.rows)
-  response
-    .status(200)
-    .json({ message: "alunos do curso.dev são pessoas acima da média" });
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query(`SHOW server_version;`);
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionsResult = await database.query(
+    `SHOW max_connections;`
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    `SHOW reserved_connections;`
+  );
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].reserved_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        opened_connections: databaseOpenedConnectionsValue,
+        max_connections: databaseMaxConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
