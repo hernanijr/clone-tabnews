f5c1e76f5d63720d85f913b3534561bf9d06a492
Hernani Junior <hernani.junior13@gmail.com>
2023-12-20 02:03:46 +0000

    let the status db agnostic

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -10,9 +10,11 @@ async function status(request, response) {
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections;
 
-  const databaseOpenedConnectionsResult = await database.query(
-    `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = 'local_db';`
-  );
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
 
